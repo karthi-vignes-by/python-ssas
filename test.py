c4be97ca86d629ec0b9c1bcb153630776f2250f4
karthi-vignes-by <65389156+karthi-vignes-by@users.noreply.github.com>
2021-05-15 13:00:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import ssas_api
 conn = ssas_api.set_conn_string(
 server='powerbi://api.powerbi.com/v1.0/myorg/_Analytics 2.0',
 db_name='carbon-footprint-new',
-username=,
-password=
+username='karthi.vignes@jda.com',
+password='Jda@1234'
 )
 
 dax_string = '''
@@ -12,6 +12,3 @@ dax_string = '''
    '''
 
 df = ssas_api.get_DAX(connection_string=conn, dax_string=dax_string)
-
-username='32f1a596-ab1e-46ad-b25a-f97f00855c54'
-password='tnKRcv7_5V-96o~InNXGx1k~Y73SDC_4O6'
